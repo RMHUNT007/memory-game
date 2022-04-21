141e7b2d7551730e68a9e47c460223787630532e
RMHUNT007 <ratulmahmud18@gmail.com>
2022-04-21 10:29:39 +0200

    fix

--- app.js
+++ app.js
@@ -1,127 +1,119 @@
 const cardArray = [
-    {
-        name: 'fries',
-        img: 'images/fries.png',
-    
-    },
-    {
-        name:'cheeseburger',
-        img : 'images/cheeseburger.png'
-    },
-    {
-        name:'hotdog',
-        img : 'images/hotdog.png'
-    },
-    {
-        name:'ice-cream',
-        img : 'images/ice-cream.png'
-    },
-    {
-        name:'milkshake',
-        img : 'images/milkshake.png'
-    },
-    {
-        name:'pizza',
-        img : 'images/pizza.png'
-    
-    },
-    {
-        name: 'fries',
-        img: 'images/fries.png',
-    
-    },
-    {
-        name:'cheeseburger',
-        img : 'images/cheeseburger.png'
-    },
-    {
-        name:'hotdog',
-        img : 'images/hotdog.png'
-    },
-    {
-        name:'ice-cream',
-        img : 'images/ice-cream.png'
-    },
-    {
-        name:'milkshake',
-        img : 'images/milkshake.png'
-    },
-    {
-        name:'pizza',
-        img : 'images/pizza.png'
-    
-    }
-]
-cardArray.sort(() => 0.5 - Math.random())
+  {
+    name: 'fries',
+    img: 'images/fries.png',
+  },
+  {
+    name: 'cheeseburger',
+    img: 'images/cheeseburger.png',
+  },
+  {
+    name: 'hotdog',
+    img: 'images/hotdog.png',
+  },
+  {
+    name: 'ice-cream',
+    img: 'images/ice-cream.png',
+  },
+  {
+    name: 'milkshake',
+    img: 'images/milkshake.png',
+  },
+  {
+    name: 'pizza',
+    img: 'images/pizza.png',
+  },
+  {
+    name: 'fries',
+    img: 'images/fries.png',
+  },
+  {
+    name: 'cheeseburger',
+    img: 'images/cheeseburger.png',
+  },
+  {
+    name: 'hotdog',
+    img: 'images/hotdog.png',
+  },
+  {
+    name: 'ice-cream',
+    img: 'images/ice-cream.png',
+  },
+  {
+    name: 'milkshake',
+    img: 'images/milkshake.png',
+  },
+  {
+    name: 'pizza',
+    img: 'images/pizza.png',
+  },
+];
+cardArray.sort(() => 0.5 - Math.random());
 
-const gridDisplay = document.querySelector('#grid')
-const  resultDisplay = document.querySelector('#result')
-let cardsChosen=[]
-let cardsChosenIds = []
-const cardsWon =[]
- 
-function createBoard () {
-    for( let i = 0 ; i < cardArray.length; i++) {
-      const card = document.createElement('img')
+const gridDisplay = document.querySelector('#grid');
+const resultDisplay = document.querySelector('#result');
+let cardsChosen = [];
+let cardsChosenIds = [];
+const cardsWon = [];
 
-      card.setAttribute ('src', 'images/blank.png')
-      card.setAttribute('data-id', i)
-      card.addEventListener('click', flipcard)
-      
-     gridDisplay.appendChild(card)
-      
-    }
+function createBoard() {
+  for (let i = 0; i < cardArray.length; i++) {
+    const card = document.createElement('img');
+
+    card.setAttribute('src', 'images/blank.png');
+    card.setAttribute('data-id', i);
+    card.addEventListener('click', flipcard);
+
+    gridDisplay.appendChild(card);
+  }
 }
-createBoard()
+createBoard();
 
-function checkMatch(){
-    const cards = document.querySelectorAll('img')
-    const optionOneId = cardsChosenIds[0]
-    const optionTwoId = cardsChosenIds[1]
+function checkMatch() {
+  const cards = document.querySelectorAll('img');
+  const optionOneId = cardsChosenIds[0];
+  const optionTwoId = cardsChosenIds[1];
 
-    console.log(cards)
-    console.log('check for Match')
-    if (optionOneId == optionTwoId) {
-        cards[optionOneId].setAttribute('src', images/blank.png)
-        cards[optionTwoId].setAttribute('src', images/blank.png)
-        alert('You Have Clicked The Same Image')
-    }
+  console.log(cards);
+  console.log('check for Match');
+  if (optionOneId == optionTwoId) {
+    cards[optionOneId].setAttribute('src', images / blank.png);
+    cards[optionTwoId].setAttribute('src', images / blank.png);
+    alert('You Have Clicked The Same Image');
+  }
 
-    if(cardsChosen[0]== cardsChosen[1]){
-        alert('You Foung a Match')
-        cards[optionOneId].setAttribute('src', images/white.png)
-        cards[optionTwoId].setAttribute('src', images/white.png)
-        cards[optionOneId].removeEventListener('click', flipcard)
-        cards[optionTwoId].removeEventListener('click', flipcard)
+  if (cardsChosen[0] == cardsChosen[1]) {
+    alert('You Foung a Match');
+    cards[optionOneId].setAttribute('src', images / white.png);
+    cards[optionTwoId].setAttribute('src', images / white.png);
+    cards[optionOneId].removeEventListener('click', flipcard);
+    cards[optionTwoId].removeEventListener('click', flipcard);
 
-        cardsWon.push(cardsChosen)
-    }else{
-        cards[optionOneId].setAttribute('src', images/blank.png)
-        cards[optionTwoId].setAttribute('src', images/blank.png)
-        alert('Sorry Try Agaun!')
+    cardsWon.push(cardsChosen);
+  } else {
+    cards[optionOneId].setAttribute('src', images / blank.png);
+    cards[optionTwoId].setAttribute('src', images / blank.png);
+    alert('Sorry Try Agaun!');
+  }
+  resultDisplay.textContent = cardsWon.length;
+  cardsChosen = [];
+  cardsChosenIds = [];
 
-    }
-    resultDisplay.textContent = cardsWon.length
-    cardsChosen =[]
-    cardsChosenIds=[]
-
-    if (cardsWon.length == cardArray.length/2){
-        resultDisplay.textContent = 'Congratulation You Found Them All!'
-
-    }
+  if (cardsWon.length == cardArray.length / 2) {
+    resultDisplay.textContent = 'Congratulation You Found Them All!';
+  }
 }
 
+function flipcard() {
+  const cardId = this.getAttribute('data-id');
+  cardsChosen.push(cardArray[cardId].name);
+  cardsChosenIds.push(cardId);
 
-function flipcard(){
-    const cardId = this.getAttribute('data-id')
-    cardsChosen.push(cardArray[cardId].name)
-    cardsChosenIds.push(cardId)
+  console.log('clicked', cardId);
+  console.log(cardsChosen);
 
-    console.log ('clicked', cardId)
-    console.log(cardsChosen)
-
-    this.setAttribute('src', cardArray[cardId].img)
-    if (cardsChosen.length === 2){
-        setTimeout(checkMatch, 500)
-    }
+  this.setAttribute('src', cardArray[cardId].img);
+  if (cardsChosen.length === 2) {
+    setTimeout(checkMatch, 500);
+  }
 }
